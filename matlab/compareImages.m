81d19dbc740da8675d250bcdb6bfaf82f4ec27c6
Rex Fung <rexfung@umich.edu>
2024-03-01 16:32:51 -0500

    Updated compareImages.m to plot the magnitude and phase of the difference image

--- matlab/compareImages.m
+++ matlab/compareImages.m
@@ -5,4 +5,6 @@ function compareImages(img_gt, img_recon)
     nexttile; im(angle(img_gt)); title('Ground Truth phase'); colorbar;
     nexttile; im(abs(img_recon)); title('Recon magnitude'); colorbar;
     nexttile; im(angle(img_recon)); title('Recon phase'); colorbar;
+    nexttile; im(abs(img_gt - img_recon)); title('Magnitude of diff img'); colorbar;
+    nexttile; im(angle(img_gt - img_recon)); title('Phase of diff img'); colorbar;
 end
