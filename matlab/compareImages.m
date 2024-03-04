6291d0da64a99b7c7f06f7b8600ff0fca02cb909
Rex Fung <rexfung@umich.edu>
2024-03-04 13:24:06 -0500

    Added MSE metric

--- matlab/compareImages.m
+++ matlab/compareImages.m
@@ -4,5 +4,9 @@ function compareImages(img_gt, img_us, img_recon)
     nexttile; im(abs(img_gt)); title('Ground Truth magnitude'); colorbar;
     nexttile; im(abs(img_us)); title('Undersampled magnitude'); colorbar;
     nexttile; im(abs(img_recon)); title('Recon magnitude'); colorbar;
-    nexttile; im(abs(img_gt - img_recon)); title('Magnitude of diff img'); colorbar;
+
+    % Differences
+    img_diff = img_gt - img_recon;
+    nexttile; im(abs(img_diff)); title('Magnitude of diff img'); colorbar;
+    MSE = mean(img_diff.^2,'all')
 end
