eee598853d06fe532cca13054308c4c2e0311e2c
Rex Fung <rexfung@umich.edu>
2024-03-04 11:40:55 -0500

    Changed compareImages() to include undersampled image (direct IFT recon) to show the effects of aliasing

--- matlab/compareImages.m
+++ matlab/compareImages.m
@@ -1,10 +1,8 @@
 %% Compare ground truth and recon'd images
-function compareImages(img_gt, img_recon)
+function compareImages(img_gt, img_us, img_recon)
     tiledlayout('flow','TileSpacing','tight');
     nexttile; im(abs(img_gt)); title('Ground Truth magnitude'); colorbar;
-    nexttile; im(angle(img_gt)); title('Ground Truth phase'); colorbar;
+    nexttile; im(abs(img_us)); title('Undersampled magnitude'); colorbar;
     nexttile; im(abs(img_recon)); title('Recon magnitude'); colorbar;
-    nexttile; im(angle(img_recon)); title('Recon phase'); colorbar;
     nexttile; im(abs(img_gt - img_recon)); title('Magnitude of diff img'); colorbar;
-    nexttile; im(angle(img_gt - img_recon)); title('Phase of diff img'); colorbar;
 end
